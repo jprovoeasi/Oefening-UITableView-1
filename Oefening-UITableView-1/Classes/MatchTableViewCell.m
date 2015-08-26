9474733608f152c40273debd5709474c090de262
Jonathan Provo <j.provo@easi.net>
2015-08-26 14:55:13 +0200

    Update.

--- Oefening-UITableView-1/Classes/MatchTableViewCell.m
+++ Oefening-UITableView-1/Classes/MatchTableViewCell.m
@@ -3,7 +3,7 @@
 //  Oefening-UITableView-1
 //
 //  Created by Jonathan Provo on 20/08/15.
-//  Copyright (c) 2015 EASI. All rights reserved.
+//  Copyright (c) 2015. All rights reserved.
 //
 
 #import "MatchTableViewCell.h"
